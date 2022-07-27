7bb61edc066f2a4339fdd745d989ac239a360a23
daton <datonarinze@gmail.com>
2022-07-27 15:09:00 +0100

    Changed some things

--- src/test/java/com/sypha/fashion_blog/FashionBlogApplicationTests.java
+++ src/test/java/com/sypha/fashion_blog/FashionBlogApplicationTests.java
@@ -1,29 +1,29 @@
-package com.sypha.fashion_blog;
-
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class FashionBlogApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-    @BeforeAll
-    static void beforeAll() {
-
-
-    }
-
-    @Test
-    void r() {
-    }
-
-    @AfterAll
-    static void afterAll() {
-        
-    }
-}
+//package com.sypha.fashion_blog;
+//
+//import org.junit.jupiter.api.AfterAll;
+//import org.junit.jupiter.api.BeforeAll;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class FashionBlogApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//    @BeforeAll
+//    static void beforeAll() {
+//
+//
+//    }
+//
+//    @Test
+//    void r() {
+//    }
+//
+//    @AfterAll
+//    static void afterAll() {
+//
+//    }
+//}
